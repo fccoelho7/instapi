52baf8c5fe4e03b1c1aafe1e300eac0c402a046b
Fabio Coelho <fccoelho7@gmail.com>
2019-07-15 22:01:39 -0300

    Initial

---
+++ src/routes.php
@@ -0,0 +1,60 @@
+<?php
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use InstagramAPI\Instagram;
+
+InstagramAPI\Instagram::$allowDangerousWebUsageAtMyOwnRisk = true;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->post('/close-friends', function (Request $request, Response $response, array $args) use ($container) {
+        $username = $request->getParam('username');
+        $password = $request->getParam('password');
+
+        try {
+            $ig = new InstagramAPI\Instagram();
+            $ig->login($username, $password);
+            $closeFriends = json_decode($ig->people->getCloseFriends(), true);
+
+            return $response->withJson(['friends' => $closeFriends['users']]);
+        } catch (\Throwable $th) {
+            return $response->withJson(['error' => $th], 400);
+        }
+    });
+
+    $app->post('/close-friends/add', function (Request $request, Response $response, array $args) use ($container) {
+        $username  = $request->getParam('username');
+        $password  = $request->getParam('password');
+        $followers = $request->getParam('friends');
+
+        try {
+            $ig = new InstagramAPI\Instagram();
+            $ig->login($username, $password);
+
+            $ig->people->setCloseFriends($followers, []);
+
+            return $response->withJson(['message' => 'Followers have added to the close friends!']);
+        } catch (\Throwable $th) {
+            return $response->withJson(['error' => $th], 400);
+        }
+    });
+
+    $app->post('/close-friends/remove', function (Request $request, Response $response, array $args) use ($container) {
+        $username  = $request->getParam('username');
+        $password  = $request->getParam('password');
+        $followers = $request->getParam('friends');
+
+        try {
+            $ig = new InstagramAPI\Instagram();
+            $ig->login($username, $password);
+
+            $ig->people->setCloseFriends([], $followers);
+
+            return $response->withJson(['message' => 'Followers have removed from the close friends!']);
+        } catch (\Throwable $th) {
+            return $response->withJson(['error' => $th], 400);
+        }
+    });
+};
